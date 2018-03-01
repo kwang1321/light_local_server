6ee9e2e0c4e59a00791bfbe08d6d2cf387951206
Qiqiang@ituBig <qguan@itu.edu>
2018-03-01 11:41:51 -0800

    modification for getting info from question mark

--- routes/rfid.js
+++ routes/rfid.js
@@ -3,9 +3,10 @@ const df = require("../common/date_format.js");
 
 module.exports = app => {
   // insert an item.
-  app.post(consts.RFIDDomin, (req, res) => {
+  app.all(consts.RFIDDomin, (req, res) => {
     console.log("post rfidservices");
     console.log("body", req.body);
+    console.log("params", req.query);
     // dh11Service.insert(req.body, "DH11", res);
     res.send({ result: req.body });
   });
