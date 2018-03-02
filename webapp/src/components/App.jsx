ec4a81043b975ce4f846f8cae33fa743eca65eb0
gqq w541 ubuntu <gqq_w541_ubuntu@itu.edu>
2018-03-02 02:22:52 -0800

    add some css files

--- webapp/src/components/App.jsx
+++ webapp/src/components/App.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter, Route } from "react-router-dom";
 import TagWriter from "./TagWriter";
 import TagTable from "./TagTable";
 
-const Landing = () => [
-  <h2 key="1">
-    <a href="/tagwriter?m=00000000000x00000">Tag Writer</a>
-  </h2>,
-  <h2 key="2">
-    <a href="/tagTable">Tag Table</a>
-  </h2>
-];
+const Landing = () => (
+  <div className="collection grey darken-2">
+    <a href="/tagwriter?m=00000000000x00000" className="collection-item">
+      Tag Writer
+    </a>
+    <a href="/tagTable" className="collection-item">
+      Tag Table
+    </a>
+  </div>
+);
 
 class App extends Component {
   render() {
