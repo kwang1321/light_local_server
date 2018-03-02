768a8702c6999fe86e16f6ddd054e433fc5c4ee4
Qiqiang@ituBig <qguan@itu.edu>
2018-03-01 16:29:28 -0800

    add RFID test route

--- routes/rfid.js
+++ routes/rfid.js
@@ -4,8 +4,10 @@ const df = require("../common/date_format.js");
 module.exports = app => {
   // insert an item.
   app.all(consts.RFIDDomin, (req, res) => {
-    console.log("post rfidservices");
+    console.log("post rfid services");
+    console.log("body type", toString.call(req.body));
     console.log("body", req.body);
+    console.log("params type", toString.call(req.query));
     console.log("params", req.query);
     // dh11Service.insert(req.body, "DH11", res);
     res.send({ result: req.body });
