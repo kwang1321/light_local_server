ed5cb981cc088dc668a22db0095ce3023d2686f6
Nathan Li <Nathan@ubuntu.com>
2018-04-01 13:48:06 -0700

    broadcast can report

--- clients/broadcast_client.js
+++ clients/broadcast_client.js
@@ -3,7 +3,16 @@ const consts = require("../config/consts");
 var PORT = consts.boradPort;
 var dgram = require("dgram");
 var client = dgram.createSocket("udp4");
+var ip = require("ip");
+var mac = require("getmac");
 
+ip.address();
+var mymacAddress;
+require("getmac").getMac(function(err, macAddress) {
+  mymacAddress = macAddress;
+  if (err) throw err;
+  console.log(macAddress);
+});
 client.on("listening", function() {
   var address = client.address();
   console.log(
@@ -15,6 +24,29 @@ client.on("listening", function() {
 client.on("message", function(message, rinfo) {
   console.log(
     "Message from: " + rinfo.address + ":" + rinfo.port + " - " + message
+  );
+
+  var ipdata = {
+    ip: ip.address(),
+    ["mac"]: mymacAddress
+  };
+
+  const request = require("request");
+
+  const URL_POST = "http://localtest.itu.edu:5000/api/v1/enddevice";
+
+  request(
+    {
+      url: URL_POST,
+      method: "POST",
+      json: ipdata
+    },
+    function(error, response, data) {
+      if (error) {
+        return console.log(error);
+      }
+      console.log("SUCCEED", data);
+    }
   );
 });
 
