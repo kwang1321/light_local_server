b42dd94ce5ebc8b220f29ecd875f66923a98b0bd
Nathan Li <Nathan@ubuntu.com>
2018-04-25 11:50:41 -0700

    fixed bugs

--- clients/sensors.js
+++ clients/sensors.js
@@ -1,4 +1,4 @@
 module.exports = [
-  { id: "dht_000000", interval: 5000 },
-  { id: "dht_000001", interval: 5000 }
+  { id: "dht_000000", interval: 3000 },
+  { id: "dht_000001", interval: 3000 }
 ];
